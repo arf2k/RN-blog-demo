ceb34597717c4802ea4d2717638ae535e6b3bde6
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-29 14:06:06 -0400

    Set up provider and context

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Blog Demo</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
   },
-});
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs Demo",
+    },
+  }
+);
+
+const styles = StyleSheet.create({});
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
