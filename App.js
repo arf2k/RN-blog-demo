6fd380c3b3cff2ea22932846a59cda254ef4905d
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-29 14:44:08 -0400

    Add Blog Post functionality works with button press

--- App.js
+++ App.js
@@ -26,5 +26,6 @@ export default () => {
     <BlogProvider>
       <App />
     </BlogProvider>
-  );
+  )
+  
 };
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const BlogContext = React.createContext()
+const BlogContext = React.createContext();
 
-export const BlogProvider = ({children}) => {
-     return <BlogContext.Provider value={5}>
-          {children}
-     </BlogContext.Provider>
-}
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, setBlogPosts] = useState([]);
 
-export default BlogContext
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #${blogPosts.length + 1}` },
+    ]);
+  };
+
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
