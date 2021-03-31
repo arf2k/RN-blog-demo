2fc216386ab82e32409bd994536be733d00bbe77
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-31 16:51:19 -0400

    Add create blog form and navigation back from index on create

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -11,11 +11,10 @@ import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost } = useContext(Context);
   return (
     <View>
       <Text>Index Screen</Text>
-      <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
@@ -37,6 +36,16 @@ const IndexScreen = ({ navigation }) => {
     </View>
   );
 };
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+    <Feather name="plus" size={30} />
+    </TouchableOpacity>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   row: {
