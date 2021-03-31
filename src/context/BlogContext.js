10e575aac9f241b92c19807d154e1dd814783b87
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-31 15:58:56 -0400

    Delete post functionality works

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,18 @@
 import React, { useReducer } from "react";
-import createDataContext from "./createDataContext"
-
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "delete_blogpost":
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     case "add_blogpost":
-      return [...state, { title: `Blog Post #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`,
+        },
+      ];
     default:
       return state;
   }
@@ -13,11 +20,18 @@ const blogReducer = (state, action) => {
 
 const addBlogPost = (dispatch) => {
   return () => {
-    dispatch({type: "add_blogpost"})
+    dispatch({ type: "add_blogpost" });
   };
 };
 
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
+};
 
-export const { Context, Provider } = createDataContext(blogReducer, {addBlogPost}, [])
-
-
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost },
+  []
+);
