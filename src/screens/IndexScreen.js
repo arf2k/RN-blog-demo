acc2b0970baef90c428adfee777fa553a6bd6d67
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-31 15:43:42 -0400

    Add post styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons"
 
 const IndexScreen = () => {
   const { state, addBlogPost} = useContext(Context);
@@ -12,13 +13,34 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Feather name="trash" style={styles.icon}/>
+          </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: "gray",
+    paddingHorizontal: 10
+  },
+  title: {
+    fontSize: 18,
+
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
