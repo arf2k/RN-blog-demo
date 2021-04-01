327dd3d4b28cdb5821c31c952d811b2e8964f447
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-04-01 17:08:36 -0400

    Update reducer and navigation to make update function work with reusable form comp

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import { Text, View, StyleSheet, TextInput, Button } from "react-native";
 
-const BlogPostForm = ({onsubmit}) => {
+const BlogPostForm = ({ onSubmit, initialValues }) => {
+  const [title, setTitle] = useState(initialValues.title);
+  const [content, setContent] = useState(initialValues.content);
 
-const [title, setTitle] = useState("");
-const [content, setContent] = useState("");
-
-
-return(
-<View>
-
+  return (
+    <View>
       <Text style={styles.label}>Enter Title:</Text>
       <TextInput
         style={styles.input}
@@ -22,29 +19,32 @@ return(
         value={content}
         onChangeText={(text) => setContent(text)}
       />
-      <Button
-        title="Save Blog Post"
-        onPress={() => onSubmit(title, content) }
-        
-      />
+      <Button title="Save Blog Post" onPress={() => onSubmit(title, content)} />
     </View>
-)
-}
+  );
+};
+
+BlogPostForm.defaultProps = {
+  initialValues: {
+    title: "",
+    content: "",
+  },
+};
 
 const styles = StyleSheet.create({
-     input: {
-          fontSize: 18,
-          borderWidth: 1,
-          borderColor: "black",
-          marginBottom: 15,
-          padding: 5,
-          margin: 5,
-        },
-        label: {
-          fontSize: 20,
-          marginBottom: 5,
-          marginLeft: 5,
-        }
-})
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+});
 
-export default BlogPostForm
+export default BlogPostForm;
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,22 +1,24 @@
 import React, { useContext } from "react";
-import {  StyleSheet} from "react-native";
-import { Context } from "../context/BlogContext"; 
-import BlogPostForm from "../components/BlogPostForm"
+import { StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
-     
-     const { state } = useContext(Context);
+  const id = navigation.getParam("id");
+  const { state, editBlogPost } = useContext(Context);
 
-     const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam("id"));
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+      }}
+    />
+  );
+};
 
+const styles = StyleSheet.create({});
 
-     
-     return(
-       <BlogPostForm/>
-     )
-}
-
-const styles = StyleSheet.create({})
-
-export default EditScreen
+export default EditScreen;
